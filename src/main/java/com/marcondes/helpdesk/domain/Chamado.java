75f7a3569ec584b37b0b10589c21ab86432070dd
Marcondes Amorim <marcondesjra@gmail.com>
2022-03-25 22:26:02 -0300

    Anotações JPA para classes de dominio

--- src/main/java/com/marcondes/helpdesk/domain/Chamado.java
+++ src/main/java/com/marcondes/helpdesk/domain/Chamado.java
@@ -1,7 +1,16 @@
 package com.marcondes.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.marcondes.helpdesk.domain.enums.Prioridade;
 import com.marcondes.helpdesk.domain.enums.Status;
 
@@ -12,16 +21,30 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String observações;
 
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado() {
--- src/main/java/com/marcondes/helpdesk/domain/Cliente.java
+++ src/main/java/com/marcondes/helpdesk/domain/Cliente.java
@@ -3,21 +3,31 @@ package com.marcondes.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.marcondes.helpdesk.domain.enums.Perfil;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Cliente extends Pessoa {
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
 }
