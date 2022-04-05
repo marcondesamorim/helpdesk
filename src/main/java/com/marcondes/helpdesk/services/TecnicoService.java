087af52eb9974816f3b35dd7bc4c1ed6cd7bcf25
Marcondes Amorim <marcondesjra@gmail.com>
2022-04-05 19:50:23 -0300

    Correção de erro de update

--- src/main/java/com/marcondes/helpdesk/services/TecnicoService.java
+++ src/main/java/com/marcondes/helpdesk/services/TecnicoService.java
@@ -48,8 +48,11 @@ public class TecnicoService {
 
     public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
         objDTO.setId(id);
-        objDTO.setSenha(encoder.encode(objDTO.getSenha()));
         Tecnico oldObj = findById(id);
+
+        if (!objDTO.getSenha().equals(oldObj.getSenha()))
+            objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+
         validaPorCpfEEmail(objDTO);
         oldObj = new Tecnico(objDTO);
         return repository.save(oldObj);
